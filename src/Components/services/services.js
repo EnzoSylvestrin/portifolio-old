4228b962de039a1d8f7fc1302cc0b635386e390f
EnzoSylvestrin <enzospavani@gmail.com>
2022-07-02 01:11:25 -0300

    pequenas correções na responsividade e criando o componente de serviços

---
+++ src/Components/services/services.js
@@ -0,0 +1,11 @@
+import './services.css';
+
+export default function Servicer() {
+    return (
+        <section class="page-light" id="services">
+            <div>
+                
+            </div>
+        </section>
+    );
+}
