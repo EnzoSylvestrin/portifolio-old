215889565db3c9f502d340c99dc6441dbbe26564
EnzoSylvestrin <enzospavani@gmail.com>
2022-07-30 17:21:06 -0300

    Arrumando detalhes nas skills e adicionando typescript e react native

--- src/Components/skills/skills.js
+++ src/Components/skills/skills.js
@@ -6,18 +6,20 @@ export default function Skills() {
     var passou = true;
 
     const restDescription = 'de experiência com essa tecnologia.'
-    const listColors = ['#83cd29', '#68217a', ' #ffd845', '#e62c2d', '#f0db4f', '#61dafb', ' #e54d26', '#3d8fc6']; 
-    const titles = ['Node.js', 'C Sharp', 'Python', 'Java', 'JavaScript', 'React', 'HTML5', 'CSS3'];
-    const progress = ['25', '50', '33.33', '110', '50', '80', '120', '100']
+    const listColors = ['#83cd29', '#68217a', ' #ffd845', '#e62c2d', '#61dafb', '#61dafb', ' #e54d26', '#3d8fc6', '#007acc', '#f0db4f']; 
+    const titles = ['Node.js', 'C Sharp', 'Python', 'Java', 'React Native', 'React', 'HTML5', 'CSS3', 'TypeScript', 'JavaScript'];
+    const progress = ['50', '50', '33.33', '110', '30', '80', '120', '100', '25', '100']
     const descriptions = 
     [
-    `3 meses ${restDescription}`,
+    `6 meses ${restDescription}`,
     `6 meses ${restDescription}`,
     `4 meses ${restDescription}`,
     `1 ano ${restDescription}`,
-    `6 meses ${restDescription}`,
+    `3 meses ${restDescription}`,
     `9 meses ${restDescription}`,
     `1 ano ${restDescription}`,
+    `1 ano ${restDescription}`,
+    `3 meses ${restDescription}`,
     `1 ano ${restDescription}`
     ];
 
@@ -51,29 +53,35 @@ export default function Skills() {
                     <p class="description">Passe o mouse/aperte em cima de alguma skill para o tempo de experiência</p>
                 </div>
                 <div class="container-skills">
-                    <div class="single-item" id='1'>
+                    <div class="single-item">
                         <i class="devicon-nodejs-plain colored" id="icon-language"></i>
                     </div>
-                    <div class="single-item" id='2'>
+                    <div class="single-item">
                         <i class="devicon-csharp-plain-wordmark colored" id="icon-language"></i>
                     </div>
-                    <div class="single-item" id='3'>
+                    <div class="single-item">
                         <i class="devicon-python-plain-wordmark colored" id="icon-language"></i>
                     </div>
-                    <div class="single-item mg-1 java">
+                    <div class="single-item mg-1">
                         <i class="devicon-java-plain-wordmark colored" id="icon-language"></i>
                     </div>
-                    <div class="single-item mg-1 js">
-                        <i class="devicon-javascript-plain colored" id="icon-language"></i>
+                    <div class="single-item mg-1">
+                        <i class="devicon-react-original colored" id="icon-language"></i>
                     </div>
-                    <div class="single-item react">
+                    <div class="single-item">
                         <i class="devicon-react-original-wordmark colored" id="icon-language"></i>
                     </div>
-                    <div class="single-item mg-1 html">
+                    <div class="single-item mg-1">
                         <i class="devicon-html5-plain-wordmark colored" id="icon-language"></i>
                     </div>
-                    <div class="single-item mg-1 css">
+                    <div class="single-item mg-1">
                         <i class="devicon-css3-plain-wordmark colored" id="icon-language"></i>
+                    </div>
+                    <div class="single-item">
+                        <i class="devicon-typescript-plain colored" id="icon-language"></i>
+                    </div>
+                    <div class="single-item">
+                        <i class="devicon-javascript-plain colored" id="icon-language"></i>
                     </div>
                 </div>
             </div>
