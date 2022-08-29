626194aad5fabd178eddfbe4e26764a7c88dc307
EnzoSylvestrin <enzospavani@gmail.com>
2022-08-28 21:55:47 -0300

    Alterações nos projetos

--- src/Components/projects/projects.js
+++ src/Components/projects/projects.js
@@ -1,6 +1,4 @@
 import './projects.css';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
 export default function Projects() {
@@ -19,12 +17,19 @@ export default function Projects() {
             <div class="container-projects">
                 <Slider {...settings}>
                     <div class="item-projects img-1">
-                        <a href="https://www.youtube.com/watch?v=FApfmscYKaY" target="_blank" rel="noopener noreferrer">
+                        <div class="img-overlay"></div>
+                        <a href="https://github.com/EnzoSylvestrin/MoveStats" target="_blank" rel="noopener noreferrer">
                             <h3>MoveStats</h3>
                             <p>Projeto finalista na febrace 2022 desenvolvido para facilitar na locomoção de deficientes visuais.</p>
                         </a>
                     </div>
                     <div class="item-projects img-2">
+                        <a href="https://enzosylvestrin.github.io/Akame-Wiki/" target="_blank" rel="noopener noreferrer">
+                            <h3>Akame Wiki</h3>
+                            <p>Site feito para testar e aprimorar meus conhecimentos com react e APIs.</p>
+                        </a>
+                    </div>
+                    <div class="item-projects img-3">
                         <a href="https://github.com/EnzoSylvestrin" target="_blank" rel="noopener noreferrer">
                             <h3>Github</h3>
                             <p>Para acessar todos meus projetos acesse meu github.</p>
