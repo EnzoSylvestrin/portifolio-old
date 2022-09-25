5b7d96da235a9bb9690583b652be45ac1c51e97a
EnzoSylvestrin <enzospavani@gmail.com>
2022-09-24 21:22:44 -0300

    Mudando imagem da tela inicial para animação do lottie

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -1,6 +1,5 @@
 import './Home.css';
 import {useTypewriter} from 'react-simple-typewriter'
-import img from './assets/programador.webp';
 import Blob from './blob.js'
 
 
@@ -24,12 +23,12 @@ export default function Home() {
           <div class="texto1">Bem vindo, meu nome é:</div>
           <div class="texto2"><span>Enzo</span></div>
           <div class="texto3">
-          &nbsp;{text}
+            &nbsp;{text}
           </div>
           <a href="https://www.linkedin.com/in/enzo-sylvestrin-336b71221" target="_blank" rel="noopener noreferrer" class="btn btn-primary">Encontre-me</a>
         </div>
         <div class="right-img">
-          <img src={img} alt="imagem programdor" class="img-home"></img>
+          <lottie-player src="https://assets8.lottiefiles.com/packages/lf20_iv4dsx3q.json"  background="transparent" class="lottie" speed="1" loop  autoplay></lottie-player>
         </div>
       </div>
     </section>
